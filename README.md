1fa3555ba42dd5b56cdb026a05f44fb3a7f5a862
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-08-12 23:24:34 +0530

    Create README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alarm Clock
+This simple web application is written using vanilla Javascript. The application show a 12 HR format Analog / Digital clock. Users are able to set multiple alarms and are alerted when an alarm time is met.
+
+## 🔗 Links
+This application is available online [here]( https://gursimratrai.github.io/Alarm-Clock/)
+ 
+
+## Screenshots
+
+![App Screenshot](https://drive.google.com//uc?export=view&id=1aWBVJ9mAvdVLQwZgjB-WESe7qDfboFHb)
+
+![App Screenshot](https://drive.google.com//uc?export=view&id=1aUgSdsmtIN6o-h5LqXq-ypTqZmOVcqTS)
+  
+  ## Major Features
+- Displays current time with one second interval updates
+- Change time period (AM/PM)
+- Ability to set multiple alarms
+- Alerts alarm when time is met
+- Submit checks inputs for validity and uniqueness
--- script.js
+++ script.js
@@ -20,38 +20,45 @@ var flag = 0;
 
 // function used for setting up an alarm
 function setAlarm() {
+  //get the value from input box
   let time = alarm.value;
+  //if time is valid / not undefined 
   if (time) {
-    let [h, m, s] = time.split(":");
+      let [h, m, s] = time.split(":");
 
-    // hh:mm:ss AM/PM
-    let timeformat = (h % 12) + 12 * (h % 12 == 0) + ":" + m + ":" + s + (h >= 12 ? " PM" : " AM");
+      // hh:mm:ss AM/PM
+      let timeformat = (h % 12) + 12 * (h % 12 == 0) + ":" + m + ":" + s + (h >= 12 ? " PM" : " AM");
 
-    //<li class="list" > HH:MM:SS AM/PM </li>
-    let li = document.createElement("li");
-    let textnode = document.createTextNode(timeformat);
-    li.setAttribute("class","list");
-    li.appendChild(textnode);
+      //<li class="list" > HH:MM:SS AM/PM </li>
+      let li = document.createElement("li");
+      let textnode = document.createTextNode(timeformat);
+      li.setAttribute("class","list");
+      li.appendChild(textnode);
 
-    //<button onClick="deleteAlarm()" id="xxxxxx" > Delete </button>
-    let btn = document.createElement("button");
-    btn.innerHTML = "X"
-    btn.setAttribute("id", Math.random().toString(36).slice(2));
-    btn.onclick = function () {
-      deleteAlarm(this.id);
-    };
+      //<button onClick="deleteAlarm()" id="xxxxxx" > Delete </button>
+      let btn = document.createElement("button");
+      btn.innerHTML = "X"
+      btn.setAttribute("id", Math.random().toString(36).slice(2));
+      btn.onclick = function () {
+        deleteAlarm(this.id);
+      };
 
-     //<li class="list" >  HH:MM:SS AM/PM <button onClick="deleteAlarm()" id="xxxxxx" > Delete </button> </li>
-    li.appendChild(btn);
-    alarmList.appendChild(li);
+      //<li class="list" > 
+      //        HH:MM:SS AM/PM <button onClick="deleteAlarm()" id="xxxxxx" > Delete </button> 
+      //</li>
+      li.appendChild(btn);
+      alarmList.appendChild(li);
   }
+
+  //reset the value
   alarm.value="";
 }
 
 // function used for deleting the selected alarm
 function deleteAlarm(id) {
-  var child = document.getElementById(id).parentNode;
-  alarmList.removeChild(child);
+  let child = document.getElementById(id);
+  let parent = child.parentNode;
+  alarmList.removeChild(parent);
 }
 
 //function to change the clock type
@@ -74,18 +81,20 @@ function showTime() {
   //(360/60) = 6
   const deg = 6;
   
+  //get the current time
   let now = new Date();
   let currentTime = now.toLocaleTimeString();
   digital.textContent = currentTime;
 
+  //rotate the hour , minute , second hand of the analog clock
   let hh = now.getHours() * 30;
   let mm = now.getMinutes() * deg;
   let ss = now.getSeconds() * deg;
-
   hour.style.transform = `rotateZ(${hh + mm / 12}deg`;
   minute.style.transform = `rotateZ(${mm}deg`;
   second.style.transform = `rotateZ(${ss}deg`;
 
+  //check if current time is equal to any time in the alarm list
   for (let listItem of list) {    
     let [time] = listItem.innerText.split("\n");
     if (time == currentTime) {
@@ -93,6 +102,7 @@ function showTime() {
     }
   }
 
+  //re-render current time with one second interval i.e. update
   setTimeout(showTime, 1000);
 }
 
